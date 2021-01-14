07d1ab0e4e1d71f0918919c136951b63b23e0a6f
Carlos Huang <thebacondragon@gmail.com>
2021-01-14 01:24:36 -0500

    added filtering for mentions and bot requests

--- channelMessageStorage.js
+++ channelMessageStorage.js
@@ -1,6 +1,15 @@
 
 const  {setIntervalAsync}  = require('set-interval-async/fixed')
+const Discord = require('discord.js');
 
+function messageOnlyContainsMentions(messageContent){
+    const userMentionsRegex = Discord.MessageMentions.USERS_PATTERN;
+    let filteredMsg = messageContent
+        .replace(new RegExp(userMentionsRegex.source ,"g"),"") //add + g afterwards
+        .trim();
+
+    return filteredMsg.length === 0;
+}
 
 
 class ChannelMessageStorage{
@@ -8,9 +17,10 @@ class ChannelMessageStorage{
         this.messages = messages; //Stores the message contents
         this.channel = channel; //Stores the channel Object
         this.mostRecentMsgId = messages[0].id;
-        setIntervalAsync(async () =>{await this.fetchNewMsgs()},2000 );
+        let intervalLength = 1000 * 60 * 5; //every 5 minutes
+        setIntervalAsync(async () =>{await this.fetchNewMsgs()},intervalLength);
 
-        //1000 * 60 * 5
+        // 1000 * 60 * 5
 
     }
 
@@ -25,10 +35,18 @@ class ChannelMessageStorage{
             let newMsgs = theMsgs.array();
 
 
+
             if (newMsgs.length === 0) {
                 return;
             }
-            this.messages.unshift(newMsgs);
+
+        let filteredMsgs = newMsgs.filter ( m =>{
+            return m !== undefined && (m.attachments.size > 0 || m.content.length > 0)
+                && !m.author.bot
+                && !m.content.match(/^(!whobot_help|!quiz)$/)
+                && !messageOnlyContainsMentions(m.content);
+        });
+            this.messages.unshift(filteredMsgs);
             this.mostRecentMsgId = newMsgs[0].id;
             // console.log("message length: " + this.messages.length);
             // console.log("Most recent id:" + this.mostRecentMsgId);
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 
 const Discord = require('discord.js')
-// const config = require('./config.json')
+const config = require('./config.json')
 const ChannelMessageStorageFile = require('./channelMessageStorage')
 
 const client = new Discord.Client();
 
+
+//There is only 1 web dyno running at once. scale it back to 1
 
 
 const prefix = "!";
@@ -14,37 +16,89 @@ function getRandomElemFromArr(array){
 
 }
 
+function messageOnlyContainsMentions(messageContent){
+    const userMentionsRegex = Discord.MessageMentions.USERS_PATTERN;
+    let filteredMsg = messageContent
+        .replace(new RegExp(userMentionsRegex.source ,"g"),"") //add + g afterwards
+        .trim();
+
+
+    return filteredMsg.length === 0;
+}
+
 async function getMessages(channel, limit = 200){
     let out = [];
     if (limit <= 100) {
         let messages = await channel.messages.fetch({ limit: limit })
         out.push(...messages.array())
     } else {
-        let rounds = (limit / 100) + (limit % 100 ? 1 : 0)
+        // let rounds = (limit / 100) + (limit % 100 ? 1 : 0)
         let last_id= ""
-        for (let x = 0; x < rounds; x++) {
-            const options = {
-                limit: 100
+        let numCycles = 0;
+        // for (let x = 0; x < rounds; x++) {
+        //     const options = {
+        //         limit: 100
+        //     }
+        //     if (last_id.length > 0) {
+        //         options.before = last_id
+        //     }
+        //     try{
+        //         const messages = await channel.messages.fetch(options,true).catch(console.error)
+        //         if(messages.array().length === 0){
+        //             break;
+        //         }
+        //
+        //         let filteredMsgs = messages.array().filter ( m =>{
+        //             return m !== undefined && (m.attachments.size > 0 ||m.content.length !== 0);
+        //         })
+        //
+        //         out.push(...filteredMsgs)
+        //         last_id = filteredMsgs[(filteredMsgs.length - 1)].id
+        //     }catch (err){
+        //         console.error(err)
+        //     }
+        //
+        // }
+
+        const options = {limit: 100};
+
+        while(out.length <limit){
+            if(last_id.length >0){
+                options.before = last_id;
             }
-            if (last_id.length > 0) {
-                options.before = last_id
+
+            //TODO: need to change in future
+            if((numCycles * 10) +1 > limit){
+                console.error("We're stuck in an infinite loop when fetching messages somehow");
+                break;
             }
+
             try{
-                const messages = await channel.messages.fetch(options,true)
-                if(messages.array().length === 0){
+                const messages = await channel.messages.fetch(options,true).catch(console.error);
+
+                let msgsArr = messages.array();
+                //exit conditions:
+                if(msgsArr.length === 0){ //No more new messages, queried all messages in the channel
                     break;
                 }
 
-                let filteredMsgs = messages.array().filter ( m =>{
-                    return m.length !== 0;
-                })
 
-                out.push(...filteredMsgs)
-                last_id = filteredMsgs[(filteredMsgs.length - 1)].id
-            }catch (err){
-                console.error(err)
+                let filteredMsgs = msgsArr.filter ( m =>{
+                    return m !== undefined && (m.attachments.size > 0 || m.content.length > 0)
+                        && !m.author.bot
+                        && !m.content.match(/^(!whobot_help|!quiz)$/)
+                        && !messageOnlyContainsMentions(m.content);
+                });
+
+
+                out.push(...filteredMsgs);
+                last_id = msgsArr[(msgsArr.length - 1)].id;
+
+            }catch (error){
+                console.error(error);
             }
 
+            numCycles++;
         }
     }
     return out
@@ -57,27 +111,6 @@ function getHelpMsg(){
         "(e.g @bob)."
 }
 
-// function quizSendAndListen(message, messages, filter){
-//     let randomMsg = getRandomElemFromArr(messages);
-//     let msgAuthor = randomMsg.author
-//     message.channel.send(randomMsg.content).then( () =>{
-//         message.channel.awaitMessages(filter,{max:1, time: 20000}).then(
-//             (collected)=>{
-//                 let firstMsg = collected.first();
-//                 if(firstMsg.mentions.members.size > 1 || firstMsg.mentions.members.size === 0){
-//                     return message.channel.send("You MUST mention only 1 person. Try again dumbass");
-//                 }
-//                 let guess = firstMsg.mentions.has(msgAuthor);
-//                 if (guess){
-//                     return message.channel.send(`Correct guess! It indeed was ${msgAuthor.username} `);
-//                 }else{
-//                     return message.channel.send(`incorrect guess! Right guess was  ${msgAuthor.username} !`);
-//                 }
-//             }).catch(() =>{
-//             message.channel.send(`Timed out, hurry up dumbass. It was ${msgAuthor.username}`);
-//         });
-//     }).catch(console.error);
-// }
 
 function quizSendAndListen(quizMessage,channelMessageManagerInstance , filter){
 
@@ -89,6 +122,11 @@ function quizSendAndListen(quizMessage,channelMessageManagerInstance , filter){
     let randomMsg = getRandomElemFromArr(messagesArr);
     let msgAuthor = randomMsg.author
     quizMessage.reply(` guess who said the message below: \n> ${randomMsg.content}`).then( () =>{
+        if(randomMsg.attachments.size >0) {
+            randomMsg.attachments.forEach( (messageAttachment)=>{
+                quizMessage.channel.send(messageAttachment.url);
+            })
+        }
         quizMessage.channel.awaitMessages(filter,{max:1, time: 20000}).then(
             (collected)=>{
                 let firstMsg = collected.first();
@@ -97,7 +135,7 @@ function quizSendAndListen(quizMessage,channelMessageManagerInstance , filter){
                 }
                 let guess = firstMsg.mentions.has(msgAuthor);
                 if (guess){
-                    return quizMessage.channel.send(`Correct guess! It indeed was ${msgAuthor.username} `);
+                    return quizMessage.channel.send(`Correct guess! It indeed was ${msgAuthor.username} `); //message.author.username
                 }else{
                     return quizMessage.channel.send(`incorrect guess! Right guess was  ${msgAuthor.username} !`);
                 }
@@ -127,62 +165,31 @@ client.on("message", (message)=>{
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
+
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
     switch (command){
+
         case "whobot_help":
+            // console.log(message);
+            // message.attachments.forEach((attachments) =>{
+            //     message.channel.send(attachments.url);
+            // })
             message.reply(getHelpMsg());
+
             break;
 
-        // case "ping":
-        //     const timeTaken = Date.now() - message.createdTimestamp;
-        //     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-        //     break;
-        //
-        //
-        // case "members":
-        //     message.guild.members.fetch().then(
-        //         (members) =>{
-        //             let membersIdlist = [];
-        //             members.forEach(guildMember =>{
-        //                 membersIdlist.push(guildMember.guild.id);
-        //                 message.channel.send(guildMember.user.username);
-        //             });
-        //             message.reply(membersIdlist);
-        //         }
-        //     ).catch(console.error);
-        //     break;
-
-
         case "quiz":
-            // const filter = (m) => m.author.id === message.author.id;
-            //
-            // if(mostRecentMsgs.length === 0){
-            //     getMessages(message.channel,800).then(
-            //         (messages) => {
-            //             mostRecentMsgs = messages.filter( (m) =>{
-            //                 return !m.author.bot && m.content.length !== 0;
-            //             });
-            //            quizSendAndListen(message, mostRecentMsgs, filter);
-            //         }).catch(console.error);
-            // }else{
-            //     quizSendAndListen(message,mostRecentMsgs, filter);
-            // }
-            // break;
 
             const filter = (m) => m.author.id === message.author.id;
 
             if(!channelMessageManager.hasChannel(message.channel)){
-                getMessages(message.channel,1000).then( (messages)=>{
-                    let mostRecentMsgsFiltered = messages.filter( m =>{
-                        return !m.author.bot && m.content.length !== 0;
-                    });
-                    channelMessageManager.addNewMessagesForChannel(message.channel,mostRecentMsgsFiltered);
+                getMessages(message.channel,150).then( (messages)=>{
+                    channelMessageManager.addNewMessagesForChannel(message.channel,messages);
                     quizSendAndListen(message,channelMessageManager,filter);
                 }).catch(console.error);
-
             }else{
                 quizSendAndListen(message,channelMessageManager,filter)
             }
@@ -192,18 +199,7 @@ client.on("message", (message)=>{
 });
 
 
-// client.login(config.BOT_TOKEN);
-//
-// client.login(config.BOT_TOKEN).then( ()=>{
-//     client.user.setPresence({
-//         status: 'online',
-//         afk: false,
-//         activity: {
-//             name: `use !whobot_help"`, type: 'PLAYING',}
-//     });
-// });
-
-client.login(process.env.BOT_TOKEN).then( ()=>{
+client.login(config.BOT_TOKEN).then( ()=>{
     client.user.setPresence({
         status: 'online',
         afk: false,
@@ -211,4 +207,13 @@ client.login(process.env.BOT_TOKEN).then( ()=>{
             name: `use !whobot_help"`, type: 'PLAYING',}
     });
 });
+
+// client.login(process.env.BOT_TOKEN).then( ()=>{
+//     client.user.setPresence({
+//         status: 'online',
+//         afk: false,
+//         activity: {
+//             name: `use !whobot_help"`, type: 'PLAYING',}
+//     });
+// });
 
