448fa4f7d4882a76c7639a0cddccce847a5b143a
Carlos Huang <thebacondragon@gmail.com>
2021-01-16 14:36:04 -0500

    configering it for heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const Discord = require('discord.js')
-const config = require('./config.json')
+// const config = require('./config.json')
 const ChannelMessageStorageFile = require('./channelMessageStorage')
 const botConfigerations = require('./botConfigerations');
 
